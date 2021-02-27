8cd24bb2a71353695c111c2ea205813826199107
renata mutalova <41615@mail.ru>
2021-02-27 03:01:34 +0300

    Stage 9

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,51 @@
+"""Prime number Game."""
+
+import prompt
+import random
+import math
+
+from brain_games.cli import welcome_user
+from brain_games.check_answer import check_answer
+
+import math
+
+
+def is_prime(number):
+    if number == 2:
+        return 'yes'
+    if number % 2 == 0 or number <= 1:
+        return 'no'
+    sqr = int(math.sqrt(number)) + 1
+    for divisor in range(3, sqr, 2):
+        if number % divisor == 0:
+            return 'no'
+    return 'yes'
+
+
+def print_random_number():
+    """Engine of the Game."""
+    number = random.randint(1, 100)
+    print('Question: {}'.format(number))
+    user_answer = prompt.string('Your answer: ')
+    correct_answer = str(is_prime(number))
+    return user_answer, correct_answer
+
+
+def main():
+    """Calculator Game function.
+    Print task three times
+    """
+    name = welcome_user()
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    counter = 0
+    n_times = 0
+    while n_times < 3:
+        user_answer, correct_answer = print_random_number()
+        counter = check_answer(user_answer, correct_answer, name, counter)
+        n_times += 1
+        if counter == 3:
+            print('Congratulations, {}!'.format(name))
+
+
+if __name__ == '__main__':
+    main()
