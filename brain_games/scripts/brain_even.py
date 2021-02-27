0350e66015d8209f3174d2a86883f7f7323dd2d4
renata mutalova <41615@mail.ru>
2021-02-27 12:24:23 +0300

    Without spases

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -23,8 +23,8 @@ def even_number(number):
 def print_random_number():
     """Engine of the Game."""
     number = random.randint(1, 1000000)
-    print('Question: ', number)
-    user_answer = prompt.string('Your answer: ')
+    print('Question:', number)
+    user_answer = prompt.string('Your answer:')
     correct_answer = even_number(number)
     return user_answer, correct_answer
 
